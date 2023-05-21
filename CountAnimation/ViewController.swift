57baf81774b2e26be57a484d9bac15ee0cdd2c2e
Minyoung-Kyung <kmy9035@naver.com>
2023-05-21 16:06:06 +0900

    commit initial CountAnimation Project

--- CountAnimation/ViewController.swift
+++ CountAnimation/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let finalNumber = 990603
+    
+    @IBOutlet weak var upLabel: CountScrollLabel!
+    
+    @IBAction func animationCountUpLabel(_ sender: Any) {
+        upLabel.animate()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        upLabel.configure(with: finalNumber)
     }
-
-
 }
 
+extension Int {
+    var formatted: String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value: self)) ?? ""
+    }
+}
